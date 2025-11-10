ec1a2e1ef65120fd82548516851894fb60a6a76d
junyoung0321 <dnfltkagudwp123@gmail.com>
2025-11-10 17:21:40 +0900

    feat: PersonInfo 추가

---
+++ src/main/java/com/example/bcsd/PersonInfo.java
@@ -0,0 +1,15 @@
+package com.example.bcsd;
+
+public class PersonInfo {
+    private String name;
+    private int age;
+
+    public PersonInfo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+
+    public int getAge() { return age; }
+}
