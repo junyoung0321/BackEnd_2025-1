fb7848a817e63479398f1e02edc03452d1cfe8ab
junyoung0321 <dnfltkagudwp123@gmail.com>
2025-11-10 17:05:41 +0900

    feat: HelloController introduce 추가

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -2,7 +2,9 @@ package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
@@ -16,5 +18,12 @@ public class HelloController {
     @GetMapping("/hello2")
     public String hello2() {
         return "hello";
+    }
+
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam(name = "name", required = false, defaultValue = "정준영") String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "introduce";
     }
 }
