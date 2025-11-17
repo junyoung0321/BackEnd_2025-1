691cf20226319afabb736569a63fe2fd1937cf17
junyoung0321 <dnfltkagudwp123@gmail.com>
2025-11-17 20:52:56 +0900

    refactor: HelloController 위치 옮기기

--- src/main/java/com/example/bcsd/Controller/HelloController.java
+++ src/main/java/com/example/bcsd/Controller/HelloController.java
@@ -1,5 +1,6 @@
-package com.example.bcsd;
+package com.example.bcsd.Controller;
 
+import com.example.bcsd.PersonInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
