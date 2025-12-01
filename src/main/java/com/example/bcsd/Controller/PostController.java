abb931d7de56cf555245fe2f59190133a528afa3
junyoung0321 <dnfltkagudwp123@gmail.com>
2025-12-01 20:15:15 +0900

    fix: PostController 메소드 추가

--- src/main/java/com/example/bcsd/Controller/PostController.java
+++ src/main/java/com/example/bcsd/Controller/PostController.java
@@ -1,4 +1,34 @@
 package com.example.bcsd.Controller;
 
+import com.example.bcsd.Model.Article;
+import com.example.bcsd.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
 public class PostController {
+
+    private final ArticleService articleService;
+
+    @Autowired
+    public PostController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/posts")
+    public String getBoardPosts(@RequestParam Long boardId, Model model) {
+        List<Article> articles = articleService.getArticlesByBoardId(boardId);
+
+        String boardName = "자유게시판";
+
+        model.addAttribute("boardName", boardName);
+        model.addAttribute("articles", articles);
+
+        return "board"; // board.html 템플릿 호출
+    }
 }
