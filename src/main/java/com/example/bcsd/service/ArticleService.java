0bf006a32a6519eeee5a335aea68adeda4bea623
junyoung0321 <dnfltkagudwp123@gmail.com>
2025-12-22 22:34:33 +0900

    fix: 구현 방식을 JPA로 전환

--- src/main/java/com/example/bcsd/service/ArticleService.java
+++ src/main/java/com/example/bcsd/service/ArticleService.java
@@ -3,21 +3,17 @@ package com.example.bcsd.service;
 import com.example.bcsd.dao.ArticleDao;
 import com.example.bcsd.Model.Article;
 import com.example.bcsd.dto.ArticleCreateRequest;
-import com.example.bcsd.exception.InvalidRequestException;
 import com.example.bcsd.exception.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class ArticleService {
 
     private final ArticleDao articleDao;
 
-    @Autowired
     public ArticleService(ArticleDao articleDao) {
         this.articleDao = articleDao;
     }
@@ -25,7 +21,6 @@ public class ArticleService {
     @Transactional
     public Article createArticle(ArticleCreateRequest request) {
         Article article = new Article();
-
         article.setAuthorId(request.getAuthorId());
         article.setBoardId(request.getBoardId());
         article.setTitle(request.getTitle());
@@ -34,36 +29,29 @@ public class ArticleService {
         return articleDao.save(article);
     }
 
-    @Transactional(readOnly = true)
     public Article getArticle(Long id) {
-        try {
-            return articleDao.findById(id);
-        } catch (EmptyResultDataAccessException e) {
+        Article article = articleDao.findById(id);
+        if (article == null) {
             throw new ResourceNotFoundException("존재하지 않는 게시글입니다. id=" + id);
         }
+        return article;
     }
 
-    @Transactional(readOnly = true)
     public List<Article> getArticlesByBoardId(Long boardId) {
         return articleDao.findAllByBoardId(boardId);
     }
 
     @Transactional
-    public Article updateArticle(Long id, Article article) {
-
-        Article existingArticle = getArticle(id);
-
-        existingArticle.setTitle(article.getTitle());
-        existingArticle.setContent(article.getContent());
-
-        return articleDao.update(existingArticle);
+    public Article updateArticle(Long id, Article updateParam) {
+        Article article = getArticle(id);
+        article.setTitle(updateParam.getTitle());
+        article.setContent(updateParam.getContent());
+        return article;
     }
 
     @Transactional
     public void deleteArticle(Long id) {
-        // 존재하는지 확인 (없으면 getArticle에서 404 터짐)
-        getArticle(id);
-
-        articleDao.delete(id);
+        Article article = getArticle(id);
+        articleDao.delete(article);
     }
 }
