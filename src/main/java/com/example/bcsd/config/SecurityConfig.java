13e9f21292449ed93cad77d47f3ed1374dd67104
junyoung0321 <dnfltkagudwp123@gmail.com>
2026-01-05 22:19:00 +0900

    feat: SecurityConfig 설정 및 Spring Boot 3 버전 대응 람다식 문법 적용

--- src/main/java/com/example/bcsd/config/SecurityConfig.java
+++ src/main/java/com/example/bcsd/config/SecurityConfig.java
@@ -3,12 +3,13 @@ package com.example.bcsd.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -16,10 +17,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/signup", "/api/login").permitAll()
-                .anyRequest().authenticated();
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/signup", "/api/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
         return http.build();
     }
 }
