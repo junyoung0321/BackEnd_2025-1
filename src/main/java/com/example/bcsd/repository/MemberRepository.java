bf39644af853f27a2d3395c74f3190436d3bcd53
junyoung0321 <dnfltkagudwp123@gmail.com>
2026-01-05 22:33:16 +0900

    feat: Spring Data JPA를 이용한 MemberRepository 인터페이스 구현

---
+++ src/main/java/com/example/bcsd/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.bcsd.repository;
+
+import com.example.bcsd.Model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
